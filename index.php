fb00bb2d2fd98335d1c7624c1716f3c97d360877
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-02 20:19:06 +0800

    Update as of 2017-12-02 20:18

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// header.php
+
+include 'partials/header.php';
+
+?>
+
+	<div class="wrapper">
+		<h1>Inclusy Bank</h1>
+
+		<a href="https://m.me/inclusy">
+		    Message us on Facebook
+		 </a>
+	</div>
+
+<?php
+// footer.php
+
+include 'partials/footer.php';
+
+?>
---
+++ partials/footer.php
@@ -0,0 +1,6 @@
+	<footer>
+		<span>Copyright &copy; 2017 by The Inceptors</span>
+	</footer>
+	
+</body>
+</html>
