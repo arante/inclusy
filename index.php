3e41268dd1e047fc5048d273302db359f2c3ad71
Billy Wilson Arante <arantebillywilson@gmail.com>
2017-12-03 03:25:07 +0800

    Update as of 2017-12-03 03:25

--- index.php
+++ index.php
@@ -5,36 +5,39 @@ include 'partials/header.php';
 
 ?>
 
-	<!-- messenger bot id: 136105417165279 -->
+	<div class="container text-center">
+		<img id="logo" src="img/logo.png">
 
-	<div class="wrapper">
-		<h1>Inclusy Bank</h1>
+		<!-- messenger bot id: 136105417165279 -->
 
-		<script>      
-			window.fbAsyncInit = function() {
-			FB.init({
-			appId      : '95100348886',
-			xfbml      : true,
-			version    : 'v2.6'
-			});
-			};
+		<script>
+	      window.fbAsyncInit = function() {
+	        FB.init({
+	          appId      : '95100348886',
+	          xfbml      : true,
+	          version    : 'v2.6'
+	        });
+	      };
 
-			(function(d, s, id){
-				var js, fjs = d.getElementsByTagName(s)[0];
-				if (d.getElementById(id)) {return;}
-				js = d.createElement(s); js.id = id;
-				js.src = "//connect.facebook.net/en_US/sdk.js";
-				fjs.parentNode.insertBefore(js, fjs);
-			}(document, 'script', 'facebook-jssdk'));      
-		</script>
+	      (function(d, s, id){
+	        var js, fjs = d.getElementsByTagName(s)[0];
+	        if (d.getElementById(id)) {return;}
+	        js = d.createElement(s); js.id = id;
+	        js.src = "//connect.facebook.net/en_US/sdk.js";
+	        fjs.parentNode.insertBefore(js, fjs);
+	      }(document, 'script', 'facebook-jssdk'));
+	    </script>
 
-		<div class="fb-messengermessageus" 
-			messenger_app_id="95100348886" 
-			page_id="136105417165279" 
-			color="blue"
-			size="large">
-		</div>
-
+	    <div class="fb-page" 
+	         data-href="https://www.facebook.com/136105417165279/" 
+	         data-tabs="messages" 
+	         data-width="400" 
+	         data-height="300" 
+	         data-small-header="true">
+	      <div class="fb-xfbml-parse-ignore">
+	        <blockquote></blockquote>
+	      </div>
+	    </div>
 	</div>
 
 <?php
--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,8 @@
-	<footer>
-		<span>Copyright &copy; 2017 by The Inceptors</span>
+	<footer class="container-fluid text-center">
+		<div class="row">
+			<span class="col-xs-12">Copyright &copy; 2017 by The Inceptors</span>
+		</div>
 	</footer>
-	
+
 </body>
 </html>
